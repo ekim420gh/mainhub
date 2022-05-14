97e8bd5fa31bb3ea41cb963464bbe500abf49213
Elbert Kim <elbert.kim@icloud.com>
2022-05-13 21:44:47 -0400

    fix time

--- hello.py
+++ hello.py
@@ -1,9 +1,19 @@
-import time
+from datetime import datetime
+from pytz import timezone
 from flask import Flask, render_template
 
-t = time.strftime("%I:%M %p")
+tz = timezone("US/Eastern")
+time = datetime.now(tz)
+year = time.year
+month = time.month
+day = time.day
+hour = time.hour
+minutes = time.minute
+
+yymmdd = "{}/{}/{} {}:{}".format(str(year), str(month), str(day), str(hour), str(minutes))
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return render_template('homepage.html', utc_dt=t)
+    return render_template('homepage.html', datetime=yymmdd)
