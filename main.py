d15e4dbfb8f4d49ca13be7a1842b043a4e251025
Elbert Kim <elbert.kim@icloud.com>
2022-05-13 21:54:06 -0400

    rename application

---
+++ main.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+from pytz import timezone
+from flask import Flask, render_template
+
+tz = timezone("US/Eastern")
+time = datetime.now(tz)
+year = time.year
+month = time.month
+day = time.day
+hour = time.hour
+minutes = time.minute
+
+yymmdd = "{}/{}/{} {}:{}".format(str(year), str(month), str(day), str(hour), str(minutes))
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return render_template('homepage.html', datetime=yymmdd)
