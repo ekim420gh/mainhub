8e66da2357c879450df68c85f1c84b91994b36e3
ekim420 <elbert.kim@icloud.com>
2022-05-18 01:56:54 +0000

    add quote feature

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
+'''
+Main script for application
+'''
+
 from datetime import datetime
 from pytz import timezone
 from flask import Flask, render_template
+
+# for scraper use
+import requests
+from flask import Flask, render_template, request
+from bs4 import BeautifulSoup
+
+url = "https://greatday.com/"
+page = requests.get(url)
+
+soup = BeautifulSoup(page.content, "html.parser")
+quote_from_greatday = soup.find(id="theQuote")
+print(type(quote_from_greatday))
+print("convert bs4.element.tag object to str object")
+quote_string = quote_from_greatday.text
+print(type(quote_string))
+print("Quote from greatday.com received")
+print("-----------")
+print(quote_from_greatday)
+print("-----------")
 
 tz = timezone("US/Eastern")
 time = datetime.now(tz)
@@ -25,3 +48,7 @@ def about():
 @app.route('/essays/why_I_began_coding')
 def why_I_began_coding():
     return render_template('why_I_began_coding.html')
+
+@app.route("/data", methods=['POST', 'GET'])
+def data():
+    return render_template("quote.html", quote=quote_string)
